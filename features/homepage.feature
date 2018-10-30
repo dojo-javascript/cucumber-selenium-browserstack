850f3996f4d5db0f449a31c600fe4238aa5c0937
Sylvain Savajols <s.savajols@gmail.com>
2018-10-30 18:07:38 +0100

    sample config

--- features/homepage.feature
+++ features/homepage.feature
@@ -8,4 +8,5 @@ Feature: Go to lexpress.fr homepage
     Then I check if "28991" pub id is in page
     Then I check if "28993" pub id is in page
     Then I check if "28999" pub id is in page
+    Then "smartAdMetaData" must have "28999,28993,29000,29001,28992,28991,28995,28980" id's
     Then I check that all sas declared in smart are present
--- features/step_definitions/homepage.js
+++ features/step_definitions/homepage.js
@@ -1,4 +1,5 @@
 const { Given, Then } = require("cucumber");
+const assert = require("cucumber-assert");
 
 Given("I go to {string}", async url => {
   await driver.get(url);
@@ -7,6 +8,19 @@ Given("I go to {string}", async url => {
 Then("I check if {string} pub id is in page", async sas_id => {
   const { By } = webdriver;
   await driver.findElement(By.css(`#sas_${sas_id}`));
+});
+
+Then("{string} must have {string} id's", async (objectName, listId) => {
+  const ids = listId.split(",");
+
+  const formatIds = await driver.executeScript(
+    `return ${objectName}.formatIds;`
+  );
+  const formatIdsList = formatIds.split(",");
+
+  await Promise.all(
+    ids.map(async id => assert.notEqual(formatIdsList.indexOf(id), -1))
+  );
 });
 
 Then("I check that all sas declared in smart are present", async () => {
